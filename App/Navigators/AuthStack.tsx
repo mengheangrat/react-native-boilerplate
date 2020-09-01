4a80ab1cfa7f201793119d73ca05e2674513d20d
Mengheang Rat <mengheangrat@icloud.com>
2020-09-01 10:59:51 +0700

    update to latest react native 0.63.0

--- App/Navigators/AuthStack.tsx
+++ App/Navigators/AuthStack.tsx
@@ -1,17 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from '../Containers/LoginScreen';
+import WelcomeScreen from '../Containers/WelcomeScreen';
 
 type AuthStackProps = {
-    LoginScreen: undefined;
+    WelcomeScreen: undefined;
 };
 
 const Stack = createStackNavigator<AuthStackProps>();
-function AuthStack() {
+
+function AuthStack(): JSX.Element {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
         </Stack.Navigator>
     );
 }
